35df1998d4a24122798fea020b90b6c3e3a96142
Mateusz <97304124+itamm15@users.noreply.github.com>
2022-11-05 22:30:00 +0100

    Linear dependent of vector system

--- helpers.py
+++ helpers.py
@@ -3,6 +3,12 @@ def transpose(matrix):
     [matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))
   ]
 
+def transpose_matrix_of_vectors(matrix):
+  return list(map(list, zip(*matrix)))
+
 def print_matrix(matrix):
   for row in matrix:
     print(row)
+
+def has_only_zeros(matrix):
+  all(int(index) == 0 for index in matrix)
--- tasks_for_studies.py
+++ tasks_for_studies.py
@@ -40,3 +40,92 @@ if not state:
 
 if not state:
   print("NO")
+
+# 2. https://www.hackerrank.com/contests/zadania-z-poprzednich-kolokwiow-2223/challenges/pp-test-linear-independence
+
+matrix_size = input()
+vectors = []
+state = True
+pairs = []
+number_of_pairs = 0
+
+for _ in range(0, int(matrix_size)):
+  vector = input().split()
+  vectors.append(vector)
+
+
+# Row by row
+for row in range(0, len(vectors)):
+  for vector in range(row + 1, len(vectors)):
+    denominator = (int(vectors[row][0]))
+    if denominator != 0:
+      r = (int(vectors[vector][0]))/denominator
+      for iterator in range(0, int(matrix_size) - 1):
+        nominator = (int(vectors[vector][iterator]))
+        denominator = int(vectors[row][iterator])
+        if denominator != 0 and nominator != 0:
+          if nominator/denominator != r:
+            state = False
+            break
+        else:
+          if (int(vectors[vector][iterator]) == 0 and int(vectors[row][iterator]) == 0) and not helpers.has_only_zeros(vectors[vector]) and not helpers.has_only_zeros(vectors[row]):
+            continue
+          else:
+            state = False
+            break
+      if state:
+        pairs.append([vectors[vector], [vectors[row]]])
+        number_of_pairs += 1
+    state = True
+
+# Row by column
+transposed_matrix = helpers.transpose_matrix_of_vectors(vectors)
+
+for row in range(0, len(vectors)):
+  for vector in range(0, len(vectors)):
+    denominator = (int(transposed_matrix[row][0]))
+    if denominator != 0:
+      r = (int(vectors[vector][0]))/denominator
+      for iterator in range(0, int(matrix_size)):
+        nominator = (int(vectors[vector][iterator]))
+        denominator = (int(transposed_matrix[row][iterator]))
+        if denominator != 0 and nominator != 0:
+          if nominator/denominator != r:
+            state = False
+            break
+        else:
+          if (int(vectors[vector][iterator]) == 0 and int(transposed_matrix[row][iterator]) == 0) and not helpers.has_only_zeros(vectors[vector]) and not helpers.has_only_zeros(transposed_matrix[row]):
+            continue
+          else:
+            state = False
+            break
+      if state:
+        pairs.append([vectors[vector], [transposed_matrix[row]]])
+        number_of_pairs += 1
+    state = True
+
+# Column by column
+for row in range(0, len(vectors)):
+  for vector in range(row + 1, len(transposed_matrix)):
+    denominator = (int(transposed_matrix[row][0]))
+    if denominator != 0:
+      r = (int(transposed_matrix[vector][0]))/denominator
+      for iterator in range(0, int(matrix_size)):
+        nominator = (int(transposed_matrix[vector][iterator]))
+        denominator = int(transposed_matrix[row][iterator])
+        if denominator != 0 and nominator != 0:
+          if nominator/denominator != r:
+            state = False
+            break
+        else:
+          if (int(transposed_matrix[vector][iterator]) == 0 and int(transposed_matrix[row][iterator]) == 0) and not helpers.has_only_zeros(transposed_matrix[vector]) and not helpers.has_only_zeros(transposed_matrix[row]):
+            continue
+          else:
+            state = False
+            break
+      if state:
+        pairs.append([transposed_matrix[vector], [transposed_matrix[row]]])
+        number_of_pairs += 1
+    state = True
+
+print(number_of_pairs)
