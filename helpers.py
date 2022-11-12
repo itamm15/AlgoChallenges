56f9b40f19ed3f75950aef6acf1bebf99807c80c
Mateusz <97304124+itamm15@users.noreply.github.com>
2022-11-12 22:23:21 +0100

    3, 4 and 5 task to be finished
    Need to think about time and space complexity

--- helpers.py
+++ helpers.py
@@ -12,3 +12,13 @@ def print_matrix(matrix):
 
 def has_only_zeros(matrix):
   all(int(index) == 0 for index in matrix)
+
+def find_the_greatest(numbers):
+  number, occurences = int(list(numbers.keys())[0]), list(numbers.values())[0]
+  for key, value in list(numbers.items())[1::]:
+    if value > occurences:
+      number = int(key)
+      occurences = value
+    elif value == occurences and int(key) < number:
+      number = int(key)
+  print(number)
